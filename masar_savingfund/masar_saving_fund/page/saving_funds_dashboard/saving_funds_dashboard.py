6410cf18ac11470681f55e0475fbf14a2af2681a
karamakcsc <karamakcsc@hotmail.com>
2022-11-29 22:46:24 +0000

    upload

--- masar_savingfund/masar_saving_fund/page/saving_funds_dashboard/saving_funds_dashboard.py
+++ masar_savingfund/masar_saving_fund/page/saving_funds_dashboard/saving_funds_dashboard.py
@@ -24,3 +24,4 @@ def get_saving_funds():
     from contr as c
     Inner Join tabEmployee e on c.employee = e.employee
     Left Join pl as p on c.employee = p.employee;""",as_dict=True)
+    #hhhhh
