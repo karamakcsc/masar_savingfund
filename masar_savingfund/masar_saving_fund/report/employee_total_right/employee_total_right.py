bac1cc517e037cb9511883560a27edf86612d23f
karamakcsc <karamakcsc@hotmail.com>
2023-11-21 13:38:47 +0300

    Joining Date

--- masar_savingfund/masar_saving_fund/report/employee_total_right/employee_total_right.py
+++ masar_savingfund/masar_saving_fund/report/employee_total_right/employee_total_right.py
@@ -17,48 +17,52 @@ def get_data(filters):
 	#SQL Query
 
 	return frappe.db.sql(f"""
-					With pl
-						as (Select tial.employee,tial.employee_name ,SUM(tial.pl_employee_contr) as total_employee_pl,SUM(tial.pl_bank_contr) as total_bank_pl,
-			            	   SUM(tial.pl_employee_contr) + SUM(tial.pl_bank_contr) as total_pl
-			            From `tabIncome Allocation Line` tial
-			            Inner Join `tabIncome Allocation` tia on tial.parent =tia.name
-						Where tia.posting_date < '{date_to}'
-							  and tia.docstatus = 1
-						Group By tial.employee,tial.employee_name),
+		With pl
+			as (Select tial.employee,tial.employee_name ,SUM(tial.pl_employee_contr) as total_employee_pl,SUM(tial.pl_bank_contr) as total_bank_pl,
+					SUM(tial.pl_employee_contr) + SUM(tial.pl_bank_contr) as total_pl
+				From `tabIncome Allocation Line` tial
+				Inner Join `tabIncome Allocation` tia on tial.parent =tia.name
+				Inner Join `tabEmployee` em on tial.employee = em.name
+				Where tia.posting_date < '{date_to}' AND tia.posting_date > em.date_of_joining
+					and tia.docstatus = 1
+				Group By tial.employee,tial.employee_name),
 
-						contr as (Select tecl.employee,tecl.employee_name ,SUM(tecl.employee_contr) total_employee_contr,SUM(tecl.bank_contr) total_bank_contr,
-			            (SUM(tecl.employee_contr)+SUM(tecl.bank_contr)) total_contr
-			            From `tabEmployee Contribution Line` tecl
-			            Inner Join `tabEmployee Contribution` tec on tecl.parent =tec.name
-						Where tec.posting_date <= '{date_to}'
-							  and tec.docstatus = 1
-						Group By tecl.employee,tecl.employee_name
-						),
-			            withdraw as (Select tsfp.employee,tsfp.employee_name ,SUM(tsfp.paid_amount) total_paid_amount
-			            From `tabSaving Fund Payment` tsfp
-			            Where tsfp.posting_date <= '{date_to}'
-			                  and tsfp.docstatus = 1
-							  and tsfp.status = 'Active'
-			            Group By tsfp.employee,tsfp.employee_name),
-					liabilty as (Select ter.employee,ter.employee_name ,ter.employee_equity_amount + ter.bank_equity_amount as liability_amount
-						From `tabEmployee Resignation` ter 
-						Where ter.posting_date <= '{date_to}'
-							AND ter.docstatus = 1
-							AND ter.resignation_date = (
-								SELECT MAX(ter.resignation_date))                 
-							Group By ter.employee,ter.employee_name
-							order by ter.resignation_date )         
+			contr as (Select tecl.employee,tecl.employee_name ,SUM(tecl.employee_contr) total_employee_contr,SUM(tecl.bank_contr) total_bank_contr,
+				(SUM(tecl.employee_contr)+SUM(tecl.bank_contr)) total_contr
+				From `tabEmployee Contribution Line` tecl
+				Inner Join `tabEmployee Contribution` tec on tecl.parent =tec.name
+				Inner Join `tabEmployee` em on tecl.employee = em.name
+				Where tec.posting_date <= '{date_to}' AND tec.posting_date > em.date_of_joining
+					and tec.docstatus = 1
+				Group By tecl.employee,tecl.employee_name
+				),
+				withdraw as (Select tsfp.employee,tsfp.employee_name ,SUM(tsfp.paid_amount) total_paid_amount
+				From `tabSaving Fund Payment` tsfp
+				Inner Join `tabEmployee` em on tsfp.employee = em.name
+				Where tsfp.posting_date <= '{date_to}' AND tsfp.posting_date > em.date_of_joining
+					and tsfp.docstatus = 1
+					and tsfp.status = 'Active'
+				Group By tsfp.employee,tsfp.employee_name),
+			liabilty as (Select ter.employee,ter.employee_name ,ter.employee_equity_amount + ter.bank_equity_amount as liability_amount
+				From `tabEmployee Resignation` ter 
+				Inner Join `tabEmployee` em on ter.employee = em.name
+				Where ter.posting_date <= '{date_to}' AND ter.posting_date > em.date_of_joining
+					AND ter.docstatus = 1
+					AND ter.resignation_date = (
+						SELECT MAX(ter.resignation_date))                 
+					Group By ter.employee,ter.employee_name
+					order by ter.resignation_date )         
 
-						Select e.employee,e.employee_name,total_employee_contr,total_bank_contr,total_contr,
-							   IFNULL(p.total_employee_pl,0)as total_employee_pl ,IFNULL(total_bank_pl,0) total_bank_pl,IFNULL(total_pl,0) total_pl,
-							   IFNULL(w.total_paid_amount,0)as total_withdraw,
-							   IFNULL(l.liability_amount,0)as liability_amount,
-							   (IFNULL(total_contr,0) + IFNULL(total_pl,0) - IFNULL(total_paid_amount,0)  - IFNULL(liability_amount,0)) as total_right
-            				from tabEmployee as e
-            				left Join contr c on e.employee = c.employee
-            				Left Join pl as p on e.employee = p.employee
-            				Left Join withdraw as w on e.employee = w.employee
-							Left Join liabilty as l on e.employee = l.employee;""")						
+				Select e.employee,e.employee_name,total_employee_contr,total_bank_contr,total_contr,
+					IFNULL(p.total_employee_pl,0)as total_employee_pl ,IFNULL(total_bank_pl,0) total_bank_pl,IFNULL(total_pl,0) total_pl,
+					IFNULL(w.total_paid_amount,0)as total_withdraw,
+					IFNULL(l.liability_amount,0)as liability_amount,
+					(IFNULL(total_contr,0) + IFNULL(total_pl,0) - IFNULL(total_paid_amount,0)  - IFNULL(liability_amount,0)) as total_right
+					from tabEmployee as e
+					left Join contr c on e.employee = c.employee
+					Left Join pl as p on e.employee = p.employee
+					Left Join withdraw as w on e.employee = w.employee
+					Left Join liabilty as l on e.employee = l.employee;""")					
 
 
 
