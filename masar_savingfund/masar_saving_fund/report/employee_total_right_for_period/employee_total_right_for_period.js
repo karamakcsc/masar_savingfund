148e5bd124c0a0f572879cb1c16fe962d7fde822
karamakcsc <karamakcsc@hotmail.com>
2023-12-05 16:53:24 +0300

    Report rights for period

---
+++ masar_savingfund/masar_saving_fund/report/employee_total_right_for_period/employee_total_right_for_period.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2023, Karama kcsc and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Employee Total Right for Period"] = {
+	"filters": [
+		{
+			"fieldname": "from",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": 80,
+			"reqd": 1,
+			"default": dateutil.year_start()
+		 },
+		{
+			"fieldname": "to",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": 80,
+			"reqd": 1,
+			"default":  dateutil.year_end()
+		}
+
+]
+};
---
+++ masar_savingfund/masar_saving_fund/report/employee_total_right_for_period/employee_total_right_for_period.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2023, Karama kcsc and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+# import frappe
+from __future__ import unicode_literals
+from frappe import _
+import frappe, erpnext, json,datetime
+from typing import Dict, List, Optional, Tuple
+
+def execute(filters=None):
+	return get_columns(), get_data(filters)
+
+def get_data(filters):
+	_from, to = filters.get('from'), filters.get('to')  # date range
+
+	if _from is not None and to is not None:
+		trans_date = (
+			datetime.datetime.strptime(_from, '%Y-%m-%d'),
+			datetime.datetime.strptime(to, '%Y-%m-%d')
+		)
+		date_rang = f"BETWEEN '{_from}' AND '{to}'"
+	else:
+		frappe.throw(_("Please provide both 'from' and 'to' filters."))
+
+	#SQL Query
+
+	return frappe.db.sql(f"""
+		With pl
+			as (Select tial.employee,tial.employee_name ,SUM(tial.pl_employee_contr) as total_employee_pl,SUM(tial.pl_bank_contr) as total_bank_pl,
+					SUM(tial.pl_employee_contr) + SUM(tial.pl_bank_contr) as total_pl
+				From `tabIncome Allocation Line` tial
+				Inner Join `tabIncome Allocation` tia on tial.parent =tia.name
+				Inner Join `tabEmployee` em on tial.employee = em.name
+				Where tia.posting_date BETWEEN '{_from}' AND '{to}' AND tia.posting_date > em.date_of_joining
+					and tia.docstatus = 1
+				Group By tial.employee,tial.employee_name),
+
+			contr as (Select tecl.employee,tecl.employee_name ,SUM(tecl.employee_contr) total_employee_contr,SUM(tecl.bank_contr) total_bank_contr,
+				(SUM(tecl.employee_contr)+SUM(tecl.bank_contr)) total_contr
+				From `tabEmployee Contribution Line` tecl
+				Inner Join `tabEmployee Contribution` tec on tecl.parent =tec.name
+				Inner Join `tabEmployee` em on tecl.employee = em.name
+				Where tec.posting_date BETWEEN '{_from}' AND '{to}'AND tec.posting_date > em.date_of_joining
+					and tec.docstatus = 1
+				Group By tecl.employee,tecl.employee_name
+				),
+				withdraw as (Select tsfp.employee,tsfp.employee_name ,SUM(tsfp.paid_amount) total_paid_amount
+				From `tabSaving Fund Payment` tsfp
+				Inner Join `tabEmployee` em on tsfp.employee = em.name
+				Where tsfp.posting_date BETWEEN '{_from}' AND '{to}' AND tsfp.posting_date > em.date_of_joining
+					and tsfp.docstatus = 1
+					and tsfp.status = 'Active'
+				Group By tsfp.employee,tsfp.employee_name),
+			liabilty as (Select ter.employee,ter.employee_name ,ter.employee_equity_amount + ter.bank_equity_amount as liability_amount
+				From `tabEmployee Resignation` ter 
+				Inner Join `tabEmployee` em on ter.employee = em.name
+				Where ter.posting_date BETWEEN '{_from}' AND '{to}' AND ter.posting_date > em.date_of_joining
+					AND ter.docstatus = 1
+					AND ter.resignation_date = (
+						SELECT MAX(ter.resignation_date))                 
+					Group By ter.employee,ter.employee_name
+					order by ter.resignation_date )         
+
+				Select e.employee,e.employee_name,total_employee_contr,total_bank_contr,total_contr,
+					IFNULL(p.total_employee_pl,0)as total_employee_pl ,IFNULL(total_bank_pl,0) total_bank_pl,IFNULL(total_pl,0) total_pl,
+					IFNULL(w.total_paid_amount,0)as total_withdraw,
+					IFNULL(l.liability_amount,0)as liability_amount,
+					(IFNULL(total_contr,0) + IFNULL(total_pl,0) - IFNULL(total_paid_amount,0)  - IFNULL(liability_amount,0)) as total_right
+					from tabEmployee as e
+					left Join contr c on e.employee = c.employee
+					Left Join pl as p on e.employee = p.employee
+					Left Join withdraw as w on e.employee = w.employee
+					Left Join liabilty as l on e.employee = l.employee;""")					
+
+def get_columns():
+	return [
+	   "Employee #: Link/Employee:150",
+	   "Employee Name: Data:200",
+	   "Total Employee Contr: Currency:200",
+	   "Total Bank Contr: Currency:200",
+	   "Total Contr: Currency:200",
+	   "Total Employee P&L: Currency:200",
+	   "Total Bank P&L: Currency:200",
+	   "Total P&L: Currency:200",
+	   "Total Withdraw: Currency:200",
+	   "Liabilty Amount: Currency:200",	   
+	   "Total Rights: Currency:200"
+
+	]
