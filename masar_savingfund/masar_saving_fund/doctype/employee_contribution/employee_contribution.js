fd0c4270e39ceddf8e5998f73f604bdcac210d2f
karamakcsc <karamakcsc@hotmail.com>
2024-04-03 15:11:02 +0300

    ishaq

--- masar_savingfund/masar_saving_fund/doctype/employee_contribution/employee_contribution.js
+++ masar_savingfund/masar_saving_fund/doctype/employee_contribution/employee_contribution.js
@@ -38,34 +38,34 @@ frappe.ui.form.on('Employee Contribution', {
 //end Default Accounts
 
 //Validate If Employee exist in the same month
-// frappe.ui.form.on('Employee Contribution', {
-// 	validate: function(frm) {
-// 		//Get the employees listed in the form
-// 		var selected_employees = new Array();
-// 		for (let e = 0; e < frm.doc.employee_contr_lines.length; e++) {
-// 				selected_employees.push(frm.doc.employee_contr_lines[e].employee);
-// 			}
+frappe.ui.form.on('Employee Contribution', {
+	validate: function(frm) {
+		//Get the employees listed in the form
+		var selected_employees = new Array();
+		for (let e = 0; e < frm.doc.employee_contr_lines.length; e++) {
+				selected_employees.push(frm.doc.employee_contr_lines[e].employee);
+			}
 
-// 			frappe.call({
-// 	        method: "masar_savingfund.masar_saving_fund.doctype.employee_contribution.employee_contribution.get_exist_employee_in_month",
-// 	        args: {
-// 	          selected_employees: selected_employees,
-// 	          date_to: frm.doc.posting_date
-// 	        },
-// 	        callback: function(r) {
-// 	          let vr_employees = frm.doc.employee_contr_lines;
-// 	          $.each(r.message, function(i, d) {
-// 	            for (let e = 0; e < vr_employees.length; e++) {
-// 	              if (vr_employees[e].employee == d.employee){
-// 									msgprint('(' + d.employee +' '+ d.employee_name + ') is already exist in another voucher for this month');
-// 									validated = false;
-// 		               }
-// 	            }
-// 	          });
-// 	        }
-// 	    });
-// 	}
-// });
+			frappe.call({
+	        method: "masar_savingfund.masar_saving_fund.doctype.employee_contribution.employee_contribution.get_exist_employee_in_month",
+	        args: {
+	          selected_employees: selected_employees,
+	          date_to: frm.doc.posting_date
+	        },
+	        callback: function(r) {
+	          let vr_employees = frm.doc.employee_contr_lines;
+	          $.each(r.message, function(i, d) {
+	            for (let e = 0; e < vr_employees.length; e++) {
+	              if (vr_employees[e].employee == d.employee){
+									msgprint('(' + d.employee +' '+ d.employee_name + ') is already exist in another voucher for this month');
+									validated = false;
+		               }
+	            }
+	          });
+	        }
+	    });
+	}
+});
 //End Validate If Employee exist in the same month
 
 
@@ -199,32 +199,32 @@ frappe.ui.form.on('Employee Contribution', {
 
 
 
-// frappe.ui.form.on('Employee Contribution', {
-//     validate: function(frm) {
-// 		// if (frm.doc.docstatus < 1){
-// 			frappe.call({
-// 				method: "masar_savingfund.masar_saving_fund.doctype.employee_contribution.employee_contribution.get_emp_contr_perc",
-// 					callback: function(r) {
-// 						frm.doc.employee_contr_lines.forEach(function(d) {
-// 							d.employee_contr = flt(d.basic_salary) * flt(r.message) /100;
-// 							d.total_contr = d.bank_contr + d.employee_contr
-// 						});
-// 						frm.refresh_field('employee_contr_lines');
-// 					}
-// 			});
-// 			frappe.call({
-// 				method: "masar_savingfund.masar_saving_fund.doctype.employee_contribution.employee_contribution.get_bank_contr_perc",
-// 					callback: function(z) {
-// 						frm.doc.employee_contr_lines.forEach(function(d) {
-// 							d.bank_contr = flt(d.basic_salary) * flt(z.message) /100;
-// 							d.total_contr = d.bank_contr + d.employee_contr
-// 						});
-// 						frm.refresh_field('employee_contr_lines');
-// 					}
-// 			});
-//     	// }
-// 	}
-// });
+frappe.ui.form.on('Employee Contribution', {
+    validate: function(frm) {
+		// if (frm.doc.docstatus < 1){
+			frappe.call({
+				method: "masar_savingfund.masar_saving_fund.doctype.employee_contribution.employee_contribution.get_emp_contr_perc",
+					callback: function(r) {
+						frm.doc.employee_contr_lines.forEach(function(d) {
+							d.employee_contr = flt(d.basic_salary) * flt(r.message) /100;
+							d.total_contr = d.bank_contr + d.employee_contr
+						});
+						frm.refresh_field('employee_contr_lines');
+					}
+			});
+			frappe.call({
+				method: "masar_savingfund.masar_saving_fund.doctype.employee_contribution.employee_contribution.get_bank_contr_perc",
+					callback: function(z) {
+						frm.doc.employee_contr_lines.forEach(function(d) {
+							d.bank_contr = flt(d.basic_salary) * flt(z.message) /100;
+							d.total_contr = d.bank_contr + d.employee_contr
+						});
+						frm.refresh_field('employee_contr_lines');
+					}
+			});
+    	// }
+	}
+});
 
 
 
